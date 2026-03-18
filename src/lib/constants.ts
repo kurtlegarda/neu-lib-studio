5f75488636e6463651b99bb61fcf4fc4cdd6356c
Kurtniel L. Toscano <kurtniel.toscano@neu.edu.ph>
2026-03-18 14:37:10 +0000

    I was able to see the history now. I will now need to modify the college

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,13 +1,149 @@
 
 export const COLLEGES = [
-  { id: "CCS", name: "College of Computer Studies", programs: ["BSCS", "BSIT", "BSIS"] },
-  { id: "CBA", name: "College of Business Administration", programs: ["BSA", "BSBA-FM", "BSBA-MM"] },
-  { id: "COE", name: "College of Engineering", programs: ["BSCE", "BSEE", "BSME"] },
-  { id: "COED", name: "College of Education", programs: ["BEED", "BSED-Eng", "BSED-Math"] },
-  { id: "CAHS", name: "College of Allied Health Sciences", programs: ["BSN", "BSP"] },
-  { id: "CAS", name: "College of Arts and Sciences", programs: ["AB-Comm", "AB-Psych"] },
-  { id: "CRIM", name: "College of Criminology", programs: ["BS-Crim"] },
-  { id: "CITHM", name: "College of International Tourism and Hospitality Management", programs: ["BSHM", "BSTM"] },
+  {
+    id: "ACC",
+    name: "Accountancy",
+    programs: ["BS-Accountancy", "BS- Accounting Information System"],
+  },
+  {
+    id: "AGRI",
+    name: "Agriculture",
+    programs: ["BS-Agriculture"],
+  },
+  {
+    id: "CAS",
+    name: "Arts and Sciences",
+    programs: [
+      "BA-Economics",
+      "BS-Psychology",
+      "BA- Public Administration",
+      "BS-Biology",
+      "AB-Political Science",
+    ],
+  },
+  {
+    id: "CBA",
+    name: "Business Administration",
+    programs: [
+      "BS-Real Estate Management",
+      "BSBA-Marketing Management",
+      "BSBA-Human Resource Development and Management",
+      "BSBA-Legal Management",
+      "BS-Entrepreneur",
+      "BSBA-Financial Management",
+    ],
+  },
+  {
+    id: "COMM",
+    name: "Communication",
+    programs: ["BA-Communication", "BA-Broadcasting", "BA-Journalism"],
+  },
+  {
+    id: "CCS",
+    name: "Computer Studies",
+    programs: [
+      "Bachelor of Library Information Science",
+      "BS-Information Technology",
+      "BS-Information Systems",
+      "BSEMC-Game Development",
+      "BSEMC-Digital Animation",
+      "BS-Computer Science",
+    ],
+  },
+  {
+    id: "CRIM",
+    name: "Criminology",
+    programs: ["BS-Criminology"],
+  },
+  {
+    id: "COED",
+    name: "Education",
+    programs: [
+      "BSEd-Filipino",
+      "BSEd-Mathematics",
+      "BSEd-Biological Sciences",
+      "BSEd-Physical Sciences",
+      "BEEd-General Sciences",
+      "BEEd-Special Education",
+      "BEEd-Content Courses",
+      "BSEd-Technology and Livelihood Education",
+      "BSED-MAPE",
+      "BEEd-Pre-School Education",
+      "BSEd-English",
+      "BSEd-Social Studies",
+    ],
+  },
+  {
+    id: "CEA",
+    name: "Engineering and Architecture",
+    programs: [
+      "BS-Astronomy",
+      "BS-Industrial Engineering",
+      "BS-Mechanical Engineering",
+      "BS-Architecture",
+      "BS-Electronics Engineering",
+      "BS-Electrical Engineering",
+      "BS-Civil Engineering",
+    ],
+  },
+  {
+    id: "CIR",
+    name: "International Relations",
+    programs: ["BA-Foreign Service"],
+  },
+  {
+    id: "LAW",
+    name: "Law",
+    programs: ["Law"],
+  },
+  {
+    id: "MEDTECH",
+    name: "Medical Technology",
+    programs: ["BS-Medical Technology"],
+  },
+  {
+    id: "MEDICINE",
+    name: "Medicine",
+    programs: ["Medicine"],
+  },
+  {
+    id: "MIDWIFERY",
+    name: "Midwifery",
+    programs: ["BS-Midwifery"],
+  },
+  {
+    id: "MUSIC",
+    name: "Music",
+    programs: [
+      "Music Preparatory and Extended Studies",
+      "BM- Choral Conducting",
+      "BM- Piano",
+      "BM-Voice",
+      "BM-Music Education",
+    ],
+  },
+  {
+    id: "NURSING",
+    name: "Nursing",
+    programs: ["BS-Nursing"],
+  },
+  {
+    id: "PT",
+    name: "Physical Therapy",
+    programs: ["BS-Physical Therapy"],
+  },
+  {
+    id: "RT",
+    name: "Respiratory Therapy",
+    programs: ["BS-Respiratory Therapy"],
+  },
 ];
 
-export const REASONS = ["Reading", "Researching", "Use of Computer", "Meeting", "Borrowing Books", "Other"];
+export const REASONS = [
+  "Reading",
+  "Researching",
+  "Use of Computer",
+  "Meeting",
+  "Borrowing Books",
+  "Other",
+];
